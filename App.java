12f055ba9fc179d2b794c0bc48bee30d9eabadea
root <root@LAPTOP-I25NFPV6.localdomain>
2026-02-26 05:26:58 +0000

    second

--- App.java
+++ App.java
@@ -1,12 +1,9 @@
-package com.example;
-
 // Simple Java application
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline!");
+        System.out.println("Social media plays an important role in my daily life. It helps me stay connected with friends, family, and colleagues, even when we are far apart. I use social media to share ideas, learn new things, and stay updated with current news and trends. It is also a useful platform for personal growth, networking, and exploring career opportunities. However, I believe in using it responsibly and maintaining a healthy balance between online and offline life. I limit my screen time and focus on positive and meaningful interactions that add value to my knowledge and overall well-being.!");
     }
 
-    // Example method for testing
     public static int add(int a, int b) {
         return a + b;
     }
