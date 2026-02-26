31b890f07b124f3f82fe9121d75e2e974f01a336
root <root@LAPTOP-I25NFPV6.localdomain>
2026-02-26 04:58:35 +0000

    file

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+// Simple Java application
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Pipeline!");
+    }
+
+    // Example method for testing
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
